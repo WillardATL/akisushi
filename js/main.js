602767797e2aaa2b2ea708eeec634f0a937d3637
Vitalii Cherenkov <WillardMoto@gmail.com>
2021-10-28 13:19:49 -0400

    add footer menu

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// script for burger menu
+let mobileBody=document.getElementById("body");
+let burgerButton=document.getElementById("burgerCheckbox");
+let mobileMenu=document.getElementById('burgerMenuList');
+
+burgerButton.addEventListener("click", function() {
+    mobileBody.classList.toggle("bodyOverflow");
+  });
+
+mobileMenu.onclick = function() {
+      burgerButton.click();
+      mobileBody.classList.remove("bodyOverflow");
+  }
